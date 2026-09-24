4ad80b089de404e2e887f7a03bbcd60a227b8160
Kaleman60 <kalemancardenas@gmail.com>
2026-09-24 17:48:11 +0200

    se ha creado una nueva carpeta con los archivos nuevos de js

---
+++ 03-Strings/JS/01-app.js
@@ -0,0 +1,13 @@
+//las cadenas de string representan texto, y se pueden definir con comillas simples o dobles
+//existen 3 formas de crear cadenas de string: con comillas simples, dobles o backticks (plantillas literales)
+//crean una cadena primitiva
+const producto1 = 'Monitor 20 pulgadas';
+const producto2 = String('Monitor 20 pulgadas');
+
+//forma menos comun
+const producto3 = new String('Monitor 20 pulgadas'); //crea un objeto de tipo string
+
+console.log(producto1);
+console.log(producto2);
+console.log(producto3);
+
