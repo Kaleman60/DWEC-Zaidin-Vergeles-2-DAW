d86890e3aad8df2fc96012f4e473b8d8c121f846
Kaleman60 <kalemancardenas@gmail.com>
2026-09-25 16:43:40 +0200

    modificaciones en los index

---
+++ 04-numeros/JS/01-app.js
@@ -0,0 +1,14 @@
+const numero = 5;
+const numero1 = 20;
+
+//creamos numeros
+
+const numero2 = 30;
+const numero3 = 20.20;
+const numero4 = .234;
+const numero5 = -8;
+
+//otra forma de crear
+
+const numero6 = new Number(29);
+console.log(numero6);
---
+++ 04-numeros/JS/02-app.js
@@ -0,0 +1,24 @@
+//utilidades de operadores
+
+let resultado;
+const numero1 = 2;
+const numero2 = 3;
+
+
+
+//suma
+resultado = numero1 + numero2;
+
+//resta
+resultado = numero1 - numero2;
+
+//multiplicacion
+resultado = numero1 * numero2;
+
+//division
+resultado = numero1 / numero2;
+
+//modulo
+resultado = numero1 % numero2;
+
+console.log(resultado);
