c7370257f3077439f0fd24fac775614fae3d44b0
Kaleman60 <kalemancardenas@gmail.com>
2026-09-25 16:57:00 +0200

    se guardan cambios en la carpeta de numeros

---
+++ 04-numeros/JS/03-app.js
@@ -0,0 +1,13 @@
+let resultado;
+
+resultado = 20+20*2;
+
+resultado = (20+20)*2;
+
+//hacemos el 20% de una cantidad resultado
+resultado = 20 * 0.2;   
+console.log(resultado);
+
+//nos hacen un descuento del 20%, pagamos el 80%
+resultado = (20+ 15+45) * 0.8;
+console.log(resultado);
