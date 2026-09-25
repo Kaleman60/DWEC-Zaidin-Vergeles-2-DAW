c2db35d37b2c2175039ded9eb88e8853987547c1
Kaleman60 <kalemancardenas@gmail.com>
2026-09-25 17:22:19 +0200

    se ha hecho la primer aactividad de la tarea

---
+++ KGAC_T01/JS/ejercicio01.js
@@ -0,0 +1,20 @@
+
+// Ejercicio 1. Tus primeras variables
+
+// Constantes (datos que no cambian)
+const nombreAlumno = "Katherine Aleman";
+const nombreModulo = "Desarrollo Web en Entorno Cliente";
+
+// Variables que pueden cambiar
+let edad = 21;
+let horasEstudio = 4;
+
+// Mostrar las cuatro variables
+console.log("Nombre:", nombreAlumno);
+console.log("Módulo:", nombreModulo);
+console.log("Edad:", edad);
+console.log("Horas de estudio a la semana:", horasEstudio);
+
+// Cambiar el valor de horas de estudio y volver a mostrarlo
+horasEstudio = 6;
+console.log("Horas de estudio actualizadas:", horasEstudio);
